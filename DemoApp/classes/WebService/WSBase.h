5f530de30cc2a1b68c9cf606e6a5f28beba36a1b
Alexandros Koutsonasios <alecnash@gmail.com>
2014-05-11 01:21:19 +0200

    last commit

--- DemoApp/classes/WebService/WSBase.h
+++ DemoApp/classes/WebService/WSBase.h
@@ -15,7 +15,6 @@
 - (NSString *)getUrlString;
 - (id)initWithParams:(WSProperty *)webServiceParameters;
 - (id)initWithParams:(WSProperty *)webServiceParameters andBaseUrlString:(NSString *)baseUrlString;
-
 - (void)performRequestwithSuccess:(void (^)(NSDictionary *))success andFailure:(void (^)(NSError *))failure;
 
 @end
