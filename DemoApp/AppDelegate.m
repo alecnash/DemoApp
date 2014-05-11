02c73053ee6ddee6b25e40d1f7048595aae4bcbb
Alexandros Koutsonasios <alecnash@gmail.com>
2014-05-11 04:44:22 +0200

    no message

--- DemoApp/AppDelegate.m
+++ DemoApp/AppDelegate.m
@@ -61,7 +61,6 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Saves changes in the application's managed object context before the application terminates.
-    [[NSURLCache sharedURLCache] removeAllCachedResponses];
     [self saveContext];
 }
 
